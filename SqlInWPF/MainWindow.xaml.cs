fcb0f4a28815a55f8e8b2fab59476eb412dcb92f
ceastld <916508835@qq.com>
2022-11-06 16:22:54 +0800

    添加项目文件。

---
+++ SqlInWPF/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FreeSql;
+using FreeSql.DataAnnotations;
+using PropertyChanged;
+
+namespace SqlInWPF
+{
+    using static Database;
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = ViewModel;
+        }
+
+        public MainWindowViewModel ViewModel = new MainWindowViewModel();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource is Button btn && btn.DataContext is Topic tp)
+            {
+                tp.Clicks++;
+            }
+        }
+    }
+
+    [AddINotifyPropertyChangedInterface]
+    public class MainWindowViewModel : DependencyObject
+    {
+        public MainWindowViewModel()
+        {
+            var list = fsql.Select<Topic>()
+                .Where(x => true)
+                .ToList();
+
+            TopicList = new ObservableCollection<Topic>(list);
+
+            var tp = list.First();
+            if (tp == null)
+            {
+                tp = new Topic() { Clicks = 10 };
+                tp.Save();
+            }
+
+            SelectedItem = list.First();
+            //next 添加了过后，立马保存
+        }
+
+        public ObservableCollection<Topic> TopicList { get; set; }
+
+
+        public Topic SelectedItem
+        {
+            get { return (Topic)GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register("SelectedItem", typeof(Topic), typeof(MainWindowViewModel), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(SelectionChangedCallback)));
+
+        private static void SelectionChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (e.OldValue as Topic)?.Close();
+            (e.NewValue as Topic)?.Open();
+        }
+
+
+    }
+    public static class Database
+    {
+        public static readonly IFreeSql fsql = new FreeSql.FreeSqlBuilder()
+                .UseConnectionString(FreeSql.DataType.Sqlite, "data source=123.db")
+                .UseAutoSyncStructure(true) //自动同步实体结构到数据库
+                .Build();
+
+        public static void CloseDatabase()
+        {
+            fsql.Dispose();
+        }
+    }
+
+    public class Topic : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [Column(IsIdentity = true, IsPrimary = true)]
+        public int Id { get; set; }
+        public int Clicks { get; set; }
+        public int CCCC => Clicks + 10;
+        public string Title { get; set; }
+        public DateTime CreateTime { get; set; }
+
+        public static IBaseRepository<Topic> Repo = fsql.GetRepository<Topic>();
+
+        public void Delete() => Repo.Delete(this);
+        public void Save() => Repo.InsertOrUpdate(this);
+
+        private int _opencount = 0;
+        public void Open()
+        {
+            if (_opencount == 0)
+            {
+                Repo.Attach(this);
+                this.PropertyChanged += Topic_PropertyChanged;
+            }
+            _opencount++;
+        }
+        public void Close()
+        {
+            _opencount = Math.Max(0, _opencount - 1);
+            if (_opencount == 0)
+            {
+                PropertyChanged -= Topic_PropertyChanged;
+            }
+        }
+
+        private void Topic_PropertyChanged(object sender, PropertyChangedEventArgs e) => Repo.Update(this);
+    }
+}
---
+++ SqlLearning/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Data.SQLite;
+using System.Data;
+using Dapper;
+using FreeSql.DataAnnotations;
+
+namespace SqlLearning
+{
+    internal class Program
+    {
+
+        public class Dog
+        {
+            [Column(IsIdentity = true, IsPrimary = true)]
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public DateTime DataAdded { get; set; }
+            public int Legs { get; set; } = 0;
+
+            public static Dog Create(string name)
+            {
+                return new Dog()
+                {
+                    Name = name,
+                    DataAdded = DateTime.Now,
+                };
+            }
+        }
+
+        
+        public class Topic
+        {
+            [Column(IsIdentity = true, IsPrimary = true)]
+            public int Id { get; set; }
+            public int Clicks { get; set; }
+            public string Title { get; set; }
+            public DateTime CreateTime { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+
+            IFreeSql fsql = new FreeSql.FreeSqlBuilder()
+                .UseConnectionString(FreeSql.DataType.Sqlite, "Data Source=123.db")
+                .UseAutoSyncStructure(true) //自动同步实体结构到数据库
+                .Build(); //请务必定义成 Singleton 单例模式
+
+            fsql.Update<Topic>(2)
+              .Set(a => a.Title == "aaabbb")
+              .Set(a => a.CreateTime == DateTime.Now)
+              .ExecuteAffrows();
+
+            //UPDATE `Topic` SET `Clicks` = ifnull(`Clicks`,0) + 1, `Time` = now() 
+            //WHERE (`Id` = 1)
+
+            fsql.Dispose();
+
+            //var repo = fsql.GetRepository<Dog>(); repo.Update(dog);
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class Database
+    {
+        public static readonly string _root = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+        public static readonly string _path = Path.Combine(_root, "123.db");
+
+        public class TestObj
+        {
+            public TestObj(int id, int score, int age)
+            {
+                Id = id;
+                Score = score;
+                Age = age;
+            }
+            public int Id { get; set; }
+            public int Score { get; set; }
+            public int Age { get; set; }
+        }
+
+        public static void test()
+        {
+            SQLiteConnection cnn = new SQLiteConnection("data source=" + _path);
+
+
+            //cnn.Open();
+            //cnn.Close();
+
+            //cnn.Execute("alter table t1 add column age int"); //添加列
+
+            //cnn.Execute("update t1 set Age = 1000 where Id = 122"); //update table
+
+            //cnn.Execute("drop table if exists t1"); //delete table
+
+            cnn.Execute("create table t1(Id int,Score int,Age int)");
+
+            var random = new Random();
+
+            for (int i = 1; i < 20; i++)
+            {
+                cnn.Execute("insert into t1 values (@Id,@Score,@Age)", new TestObj(i, random.Next(1, 100), random.Next(1, 100))); //插入
+            }
+        }
+
+        public static void LookUpTable()
+        {
+            SQLiteConnection cn = new SQLiteConnection("data source=" + _path);
+            cn.Open();
+            SQLiteCommand cmd = cn.CreateCommand();
+
+            cmd.CommandText = "PRAGMA table_info('t1')";
+
+            //写法二：用DataReader，这个效率高些
+            SQLiteDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader[i]},");
+                }
+                Console.WriteLine();
+            }
+            reader.Close();
+        }
+    }
+}
